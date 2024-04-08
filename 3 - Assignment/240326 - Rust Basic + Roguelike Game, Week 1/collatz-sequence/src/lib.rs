616b76291bbb78e1be900368b06b672e654071b6
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-04-09 01:51:58 +0900

    feat: `collatz-sequence` implementation

--- 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/collatz-sequence/src/lib.rs
+++ 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/collatz-sequence/src/lib.rs
@@ -1,6 +1,16 @@
 /// Determine the length of the collatz sequence beginning at `n`.
 pub fn collatz_length(mut n: i32) -> u32 {
-    todo!("Implement this")
+    let mut result: u32 = 1;
+    while n != 1 {
+        if n % 2 == 0 {
+            n = n / 2;
+            result += 1;
+            continue;
+        }
+        n = 3 * n + 1;
+        result += 1;
+    }
+    result
 }
 
 #[cfg(test)]
